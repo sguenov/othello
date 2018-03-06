d1c9640b7f5b174ad8bae4f07830c8a29a59f318
sguenov <sguenov@hotmail.com>
2018-03-05 23:51:38 -0800

    updated tests

--- testminimax.cpp
+++ testminimax.cpp
@@ -25,7 +25,7 @@ int main(int argc, char *argv[]) {
     // Initialize player as the white player, and set testing_minimax flag.
     Player *player = new Player(WHITE);
     player->testingMinimax = true;
-
+    player->setBoard(boardData);
 
     /**
      * TODO: Write code to set your player's internal board state to the
