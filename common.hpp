ad5918aeb2ea6abf9da369a3d1382cf0fe021644
sguenov <sguenov@hotmail.com>
2018-03-12 00:12:39 -0700

    Added Complex Move class -Sarah

--- common.hpp
+++ common.hpp
@@ -9,17 +9,44 @@ class Move {
    
 public:
     int x, y;
-    Move(int x, int y) {
+    double score;
+    Move(int x, int y, double score = 0) {
         this->x = x;
-        this->y = y;        
+        this->y = y;
+        this->score = score;        
     }
     ~Move() {}
 
     int getX() { return x; }
     int getY() { return y; }
-
+    double getScore(){return score;}
     void setX(int x) { this->x = x; }
     void setY(int y) { this->y = y; }
+    void setScore(double score){this->score = score;}
 };
+class ComplexMove {
+   
+public:
+    int x, y;
+    double score;
+    ComplexMove * next;
+    Move(int x, int y, double score = 0, ComplexMove * next = nullptr) {
+        this->x = x;
+        this->y = y;
+        this->score = score;
+        this->next = next;        
+    }
+    ~Move() {}
 
+    int getX() { return x; }
+    int getY() { return y; }
+    double getScore(){return score;}
+    Move * peek(){
+        return next;
+    }
+    void setX(int x) { this->x = x; }
+    void setY(int y) { this->y = y; }
+    void setScore(double score){this->score = score;}
+    void setNext(ComplexMove * next){this->next = next;}
+};
 #endif
